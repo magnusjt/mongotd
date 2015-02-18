70bbafa580a8879b9c764952d33f2ecfad3975d0
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-02-18 15:02:33 +0100

    Added a plain-old-data object for counter values

---
+++ src/CounterValue.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mongotd;
+
+class CounterValue{
+    public $sid;
+    public $nid;
+    public $datetime;
+    public $value;
+
+    public function __construct($sid, $nid, \DateTime $datetime, $value){
+        $this->sid = $sid;
+        $this->nid = $nid;
+        $this->datetime = $datetime;
+        $this->value = $value;
+    }
+}
