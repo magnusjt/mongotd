7071c8149197ac3f29f70b72c13a7d0206b3ae80
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-02-28 17:48:46 +0100

    The resolution enum now has values in seconds rather than minutes

--- src/Resolution.php
+++ src/Resolution.php
@@ -1,9 +1,9 @@
 <?php namespace Mongotd;
 
 class Resolution{
-    const MINUTE = 1;
-    const FIVE_MINUTES = 5;
-    const FIFTEEEN_MINUTES = 15;
-    const HOUR = 60;
-    const DAY = 1440;
+    const MINUTE = 60;
+    const FIVE_MINUTES = 300;
+    const FIFTEEEN_MINUTES = 900;
+    const HOUR = 3600;
+    const DAY = 86400;
 }
