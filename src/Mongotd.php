7a638cc1c9306407b838e88bfdbec93fa70ca2d7
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-02-28 12:31:00 +0100

    Added comments, fixed issue where interval wasnt sent to deltaconverte constructor

--- src/Mongotd.php
+++ src/Mongotd.php
@@ -18,13 +18,25 @@ class Mongotd{
         $this->logger = $logger;
     }
 
-    public function getInserter($resolution = Resolution::FIFTEEEN_MINUTES){
-        $gauge_inserter = new GaugeInserter($this->conn, $resolution, $this->logger);
-        $delta_converter = new DeltaConverter($this->conn, $this->logger);
+    /**
+     * @param int $interval Number of seconds between each collected sample. Default 300 seconds = 5 minutes
+     * @return Inserter
+     *
+     * Interval is the expected number of seconds between each collected sample.
+     * Incremental values are scaled to reflect this. Also, space in database is
+     * preallocated for values at the interval, and the time for a data point is rounded
+     * down to the nearest interval point.
+     */
+    public function getInserter($interval = 300){
+        $gauge_inserter = new GaugeInserter($this->conn, $interval, $this->logger);
+        $delta_converter = new DeltaConverter($this->conn, $interval, $this->logger);
         $anomaly_detector = new AnomalyDetector($this->conn);
         return new Inserter($gauge_inserter, $delta_converter, $anomaly_detector);
     }
 
+    /**
+     * @return Retriever
+     */
     public function getRetriever(){
         $aggregator_sub_hour = new AggregatorSubHour($this->logger);
         $aggregator_hour = new AggregatorHour($this->logger);
@@ -34,6 +46,9 @@ class Mongotd{
         return new Retriever($this->conn, $aggregator, $this->logger);
     }
 
+    /**
+     * Adds indexes to collections. Should be run only once.
+     */
     public function ensureIndexes(){
         $this->conn->col('cv_prev')->ensureIndex(array('sid' => 1, 'nid' => 1), array('unique' => true));
         $this->conn->col('cv')->ensureIndex(array('mongodate' => 1, 'sid' => 1, 'nid' => 1), array('unique' => true));
