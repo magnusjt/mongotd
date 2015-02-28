3b433e2ae496da67ec7d2a27db19076cf2582c51
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-02-28 17:47:18 +0100

    Removed aggregator classes. Not needed anymore as it is done in the retriever class. Also started on method for pruning old data

--- src/Mongotd.php
+++ src/Mongotd.php
@@ -23,11 +23,11 @@ class Mongotd{
      * @return Inserter
      *
      * Interval is the expected number of seconds between each collected sample.
-     * Incremental values are scaled to reflect this. Also, space in database is
-     * preallocated for values at the interval, and the time for a data point is rounded
+     * Incremental values are scaled to represent the change in interval seconds.
+     * Space in database is preallocated for values at the interval, and the time for a data point is rounded
      * down to the nearest interval point.
      */
-    public function getInserter($interval = 300){
+    public function getInserter($interval = Resolution::FIVE_MINUTES){
         $gaugeInserter = new GaugeInserter($this->conn, $interval, $this->logger);
         $deltaConverter = new DeltaConverter($this->conn, $interval, $this->logger);
         $anomalyDetector = new AnomalyDetector($this->conn);
@@ -38,12 +38,7 @@ class Mongotd{
      * @return Retriever
      */
     public function getRetriever(){
-        $aggregatorSubHour = new AggregatorSubHour($this->logger);
-        $aggregatorHour = new AggregatorHour($this->logger);
-        $aggregatorDay = new AggregatorDay($this->logger);
-
-        $aggregator = new Aggregator($this->conn, $aggregatorDay, $aggregatorHour, $aggregatorSubHour);
-        return new Retriever($this->conn, $aggregator, $this->logger);
+        return new Retriever($this->conn, $this->logger);
     }
 
     /**
@@ -53,5 +48,13 @@ class Mongotd{
         $this->conn->col('cv_prev')->ensureIndex(array('sid' => 1, 'nid' => 1), array('unique' => true));
         $this->conn->col('cv')->ensureIndex(array('mongodate' => 1, 'sid' => 1, 'nid' => 1), array('unique' => true));
         $this->conn->col('acache')->ensureIndex(array('sid' => 1, 'nid' => 1), array('unique' => true));
+    }
+
+    public function pruneOldData(){
+        $this->conn->col('cv_prev')->remove(array(
+            'mongodate' => array(
+                '$lt' => new \MongoDate((new \DateTime('-1 day'))->getTimestamp())
+            )
+        ));
     }
 }
