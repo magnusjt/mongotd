024ae42915e2fae7526c25c1d0cfc91be409dca7
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-02-28 17:48:02 +0100

    Total rework of retriever. Simplified aggregation and timezone handling

--- src/Retriever.php
+++ src/Retriever.php
@@ -9,40 +9,120 @@ class Retriever{
     /** @var  LoggerInterface */
     private $logger;
 
-    /** @var  Aggregator */
-    private $aggregator;
-
     /**
      * @param $conn Connection
-     * @param $aggregator Aggregator
      * @param $logger LoggerInterface
      */
-    public function __construct($conn, $aggregator, $logger = NULL){
+    public function __construct($conn, $logger = NULL){
         $this->conn       = $conn;
         $this->logger     = $logger;
-        $this->aggregator = $aggregator;
     }
 
     /**
      * @param $sid         int|string
      * @param $nid         int|string
-     * @param $start      \DateTime
-     * @param $end        \DateTime
+     * @param $start       \DateTime
+     * @param $end         \DateTime
      * @param $resolution  int
      * @param $aggregation int
      * @param $padding     mixed
      *
      * @return array
+     * @throws \Exception
      */
     public function get($sid, $nid, $start, $end, $resolution = Resolution::FIFTEEEN_MINUTES, $aggregation = Aggregation::SUM, $padding = false){
         $targetTimezone = $start->getTimezone();
 
-        $start = clone $start;
-        $end = clone $end;
-        $this->adjustDatetimesToResolution($resolution, $start, $end);
+        if($resolution == Resolution::MINUTE){
+            $start = DateTimeHelper::clampToMinute($start);
+            $end = DateTimeHelper::clampToMinute($end);
+            $interval = '1 minute';
+            $clampFunction = '\Mongotd\DateTimeHelper::clampToMinute';
+        }else if($resolution == Resolution::FIVE_MINUTES){
+            $start = DateTimeHelper::clampToFiveMin($start);
+            $end = DateTimeHelper::clampToFiveMin($end);
+            $interval = '5 minutes';
+            $clampFunction = '\Mongotd\DateTimeHelper::clampToFiveMin';
+        }else if($resolution == Resolution::FIFTEEEN_MINUTES){
+            $start = DateTimeHelper::clampToFifteenMin($start);
+            $end = DateTimeHelper::clampToFifteenMin($end);
+            $interval = '15 minutes';
+            $clampFunction = '\Mongotd\DateTimeHelper::clampToFifteenMin';
+        }else if($resolution == Resolution::HOUR){
+            $start = DateTimeHelper::clampToHour($start);
+            $end = DateTimeHelper::clampToHour($end);
+            $interval = '1 hour';
+            $clampFunction = '\Mongotd\DateTimeHelper::clampToHour';
+        }else if($resolution == Resolution::DAY){
+            $start = DateTimeHelper::clampToDay($start);
+            $end = DateTimeHelper::clampToDay($end);
+            $interval = '1 day';
+            $clampFunction = '\Mongotd\DateTimeHelper::clampToDay';
+        }else{
+            throw new \Exception('blarg!');
+        }
 
-        $valsByDate = $this->aggregator->aggregate($sid, $nid, $start, $end,  $resolution, $aggregation, $targetTimezone);
-        $valsByDate = $this->filterAndPadVals($valsByDate, $resolution, $start, $end, $padding);
+        $end->add(\DateInterval::createFromDateString($interval));
+        $dateperiod = new \DatePeriod($start, \DateInterval::createFromDateString($interval), $end);
+
+        $cursor = $this->conn->col('cv')->find(array('sid' => $sid, 'nid' => $nid, 'mongodate' => array(
+                                                                        '$gte' => new \MongoDate($start->setTimezone(new \DateTimeZone('UTC'))->setTime(0, 0, 0)->getTimestamp()),
+                                                                        '$lte' => new \MongoDate($end->setTimezone(new \DateTimeZone('UTC'))->setTime(0, 0, 0)->getTimestamp()))
+                                               ), array('mongodate' => 1, 'vals' => 1));
+
+        $valsByDate = $this->getValsByDate($cursor, $aggregation, $clampFunction, $targetTimezone);
+
+        foreach($dateperiod as $datetime){
+            $dateStr = $datetime->format('Y-m-d H:i:s');
+            if(!isset($valsByDate[$dateStr])){
+                $valsByDate[$dateStr] = $padding;
+            }
+        }
+
+        return $valsByDate;
+    }
+
+    private function getValsByDate($cursor, $aggregation, $clampFunction, $targetTimezone){
+        $valsByDate = array();
+        $countsByDate = array();
+        foreach($cursor as $doc){
+            $timestamp = $doc['mongodate']->sec;
+            foreach($doc['vals'] as $seconds => $value){
+                if($value === null){
+                    continue;
+                }
+
+                // Clamp the datetime so it is unique for the current resolution
+                // Also convert to local timezone so date strings are local.
+                $datetime = new \DateTime('@'.($timestamp+$seconds));
+                $datetime->setTimezone($targetTimezone);
+                $dateStr = call_user_func($clampFunction, $datetime)->format('Y-m-d H:i:s');
+
+                if(isset($valsByDate[$dateStr])){
+                    if($aggregation == Aggregation::SUM){
+                        $valsByDate[$dateStr] += $value;
+                    }else if($aggregation == Aggregation::AVG){
+                        $valsByDate[$dateStr] += $value;
+                    }else if($aggregation == Aggregation::MAX){
+                        $valsByDate[$dateStr] = max($valsByDate[$dateStr], $value);
+                    }else if($aggregation == Aggregation::MIN){
+                        $valsByDate[$dateStr] = min($valsByDate[$dateStr], $value);
+                    }
+                }else{
+                    $valsByDate[$dateStr] = $value;
+                    $countsByDate[$dateStr] = 0;
+                }
+
+                $countsByDate[$dateStr]++;
+            }
+        }
+
+        if($aggregation == Aggregation::AVG){
+            foreach($valsByDate as $dateStr => $value){
+                $valsByDate[$dateStr] /= $countsByDate[$dateStr];
+            }
+        }
+
         return $valsByDate;
     }
 
@@ -67,51 +147,5 @@ class Retriever{
         }
 
         return $docs;
-    }
-
-    /**
-     * @param $valsByDate array
-     * @param $resolution int
-     * @param $start \DateTime
-     * @param $end \DateTime
-     * @param $padding mixed
-     * @return array
-     */
-    private function filterAndPadVals($valsByDate, $resolution, $start, $end, $padding){
-        $interval = $resolution . " minutes";
-        $endAdjusted = clone $end;
-        $endAdjusted->modify("+ " . $interval);
-        $dateperiod = new \DatePeriod($start, \DateInterval::createFromDateString($interval), $endAdjusted);
-
-        $valsByDatePadded = array();
-        foreach($dateperiod as $datetime){
-            $dateStr = $datetime->format('Y-m-d H:i:00');
-            if(isset($valsByDate[$dateStr]) and $valsByDate[$dateStr] !== false){
-                $valsByDatePadded[$dateStr] = $valsByDate[$dateStr];
-            }else{
-                $valsByDatePadded[$dateStr] = $padding;
-            }
-        }
-
-        return $valsByDatePadded;
-    }
-
-    /**
-     * @param $resolution int
-     * @param $start \DateTime
-     * @param $end \DateTime
-     */
-    private function adjustDatetimesToResolution($resolution, $start, $end){
-        if($resolution == Resolution::DAY){
-            $start->setTime(0, 0, 0);
-            $end->setTime(0, 0, 0);
-        }else if($resolution <= Resolution::HOUR){
-            $startMin = (int)$start->format('i');
-            $endMin   = (int)$end->format('i');
-            $startMin -= $startMin % $resolution;
-            $endMin -= $endMin % $resolution;
-            $start->setTime($start->format('H'), $startMin, 0);
-            $end->setTime($end->format('H'), $endMin, 0);
-        }
     }
 }
