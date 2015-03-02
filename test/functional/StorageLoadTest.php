0e6a88787cc28279c410638399c38ab3fc5fe6dc
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-03-02 12:47:47 +0100

    Condensed load tests into single file with a json config file

--- test/functional/StorageLoadTest.php
+++ test/functional/StorageLoadTest.php
@@ -1,50 +1,70 @@
 <?php
-/*
- * Insert 1 days of 5-min values every iteration. Does this with a bunch of sids/nids.
- * Many sids/nids/datetimes, so should be fairly realistic situation.
- *
- * Simulates 60 days of data with 4000 "sensors" (20 nodes, 200 sensors), once every 5 minutes
- * 60*4000*12*24 = about 70M data points total
- */
 require __DIR__ . '/../../vendor/autoload.php';
-date_default_timezone_set('Europe/Oslo');
-$conn = new \Mongotd\Connection('localhost', 'test', 'test');
+
+$config = json_decode(file_get_contents('loadTestConfig.json'), true);
+date_default_timezone_set($config['timezone']);
+$start   = new \DateTime($config['starttime']);
+$conn    = new \Mongotd\Connection($config['dbhost'], $config['dbname'], $config['dbprefix']);
 $mongotd = new \Mongotd\Mongotd($conn);
 $conn->dropDb();
 $mongotd->ensureIndexes();
-$inserter = $mongotd->getInserter(\Mongotd\Resolution::FIVE_MINUTES);
-
-$start = new \DateTime('2015-02-28 00:00:00');
-$iteration = 1;
+$inserter = $mongotd->getInserter($config['insertIntervalInSeconds']);
+$retriever = $mongotd->getRetriever();
+$currIteration = 1;
 $totalInserts = 0;
-while($iteration <= 60){
+
+while($currIteration <= $config['nIterations']){
+    echo "Starting iteration $currIteration...\n";
     $end = clone $start;
-    $end->add(DateInterval::createFromDateString('1 day'));
-    $dateperiod = new \DatePeriod($start, DateInterval::createFromDateString('5 minutes'), $end);
+    $end->add(DateInterval::createFromDateString($config['daysPerIteration']));
+    $dateperiod = new \DatePeriod($start, DateInterval::createFromDateString($config['insertIntervalInSeconds'] . ' seconds'), $end);
 
     $timerStart = microtime(true);
-
-    $inserts = 0;
+    $inserts    = 0;
     foreach($dateperiod as $datetime){
-        for($nid = 1; $nid <= 20; $nid++){
-            for($sid = 1; $sid <= 200; $sid++){
-                $inserter->add(1, 1, $datetime, rand());
+        echo "Date/Time: " . $datetime->format('Y-m-d H:i:s') . "\r";
+        for($nid = 1; $nid <= $config['nNids']; $nid++){
+            for($sid = 1; $sid <= $config['nSids']; $sid++){
+                $inserter->add($sid, $nid, $datetime, rand(), $config['valuesAreIncremental']);
                 $inserts++;
+                $totalInserts++;
             }
         }
 
         $inserter->insert();
     }
 
-    $totalInserts += $inserts;
+    $insertTime = (microtime(true) - $timerStart);
+    $peakMem = memory_get_peak_usage(true) / (1024 * 1000);
+    $insertRate = $inserts / $insertTime;
+    $stats = $conn->db()->command(array('dbstats' => 1, 'scale' => 1000*1024));
 
-    $totalTime = (microtime(true) - $timerStart);
-    echo 'Iteration: ' . $iteration . "\n";
-    echo 'Time taken: ' . $totalTime . ' seconds' . "\n";
-    echo 'Total inserts: ' . $totalInserts . "\n";
-    echo 'Inserts/sec: ' . $inserts/$totalTime . "\n";
-    echo 'Peak memory used: ' . memory_get_peak_usage(true)/(1024*1000)  . ' MB' . "\n\n";
+    echo "\n" . '------------- Inserts -------------' . "\n";
+    echo 'Time taken:       ' . $insertTime . ' seconds'    . "\n";
+    echo 'Total inserts:    ' . $totalInserts               . "\n";
+    echo 'Inserts/sec:      ' . $insertRate                 . "\n";
+    echo 'DB data size:     ' . $stats['dataSize'] . 'MB'   . "\n";
 
-    $start->add(DateInterval::createFromDateString('10 days'));
-    $iteration++;
+    if($config['doRetrievalTest']){
+        echo '------------- Retrievals -------------' . "\n";
+        $timerStart = microtime(true);
+        $retrievals = 0;
+        for($nid = 1; $nid <= $config['nNids']; $nid++){
+            for($sid = 1; $sid <= $config['nSids']; $sid++){
+                $retriever->get($nid, $sid, $start, $end, $config['retrieveResolution'], $config['retrieveAggregation']);
+                $retrievals++;
+            }
+        }
+
+        $retrievalTime = (microtime(true) - $timerStart);
+
+        echo 'Time taken:      ' . $retrievalTime             . "\n";
+        echo 'Retrievals done: ' . $retrievals                . "\n";
+        echo 'Retrievals/src:  ' . $retrievals/$retrievalTime . "\n";
+    }
+
+    $start->add(DateInterval::createFromDateString($config['daysPerIteration']));
+    $currIteration++;
+    echo 'Peak memory used: ' . $peakMem . ' MB' . "\n";
+    echo "\n";
 }
