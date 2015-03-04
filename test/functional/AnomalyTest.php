c1cb00d12c8bdb4f6c93a0f60767e59d339e4c12
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-03-04 11:08:37 +0100

    Anomaly test now reports more statistics, and has the option to persist an anomaly for a period of time

--- test/functional/AnomalyTest.php
+++ test/functional/AnomalyTest.php
@@ -14,34 +14,62 @@ $signalGenerator = new \Mongotd\SignalGenerator();
 $series = $signalGenerator->generateSineDailyPeriodWithNoise($config['nDays'], $config['insertIntervalInSeconds'], $config['noiseRate']);
 
 $iteration = 1;
-$resultText = '';
+
+$totalAnomalies = 0;
+$totalBoth = 0;
+$hits = 0;
+$misses = 0;
+$falsesPositives = 0;
+$persist = 0;
+
 foreach($series as $data){
     echo 'Progress: ' . $iteration . '/' . count($series) . "\r";
     $iteration++;
-
     $expectedAnomaly = false;
+
     if(rand()%1000 == 0){
+        $persist = $config['anomalyPersistIntervals'];
+    }
+
+    if($persist > 0){
+        $persist--;
         $expectedAnomaly = true;
+        $totalAnomalies++;
         $data['value'] *= 2;
     }
 
     $inserter->add(1, 1, $data['datetime'], $data['value']);
     $inserter->insert();
 
-    $resultText .= $data['datetime']->format('Y-m-d H:i') . ' - ' . $data['value'];
-
     $datetimeStartInterval = clone $data['datetime'];
-    $datetimeStartInterval->sub(\DateInterval::createFromDateString($config['insertIntervalInSeconds'] . ' seconds'));
+    $datetimeStartInterval->sub(\DateInterval::createFromDateString(($config['insertIntervalInSeconds']-1) . ' seconds'));
     $anomalies = $retriever->getAnomalies($datetimeStartInterval, $data['datetime']);
-    if(count($anomalies) > 0){
-        $resultText .= ' Anomaly!';
-    }
-    if($expectedAnomaly){
-        $resultText .= ' Anomaly Expected!';
-    }
 
-    $resultText .= "\n";
+    $isAnomaly = count($anomalies) > 0;
+
+    if($isAnomaly or $expectedAnomaly){
+        echo $data['datetime']->format('Y-m-d H:i') . ' - ' . $data['value'];
+        if($isAnomaly){
+            echo ' Detected anomaly! (predicted avg ' . $anomalies[0]['avg'] . ')';
+        }
+        if($expectedAnomaly){
+            echo ' Actual anomaly!';
+        }
+
+        $totalBoth++;
+        if($isAnomaly and $expectedAnomaly){
+            $hits++;
+        }else if($isAnomaly){
+            $falsesPositives++;
+        }else if($expectedAnomaly){
+            $misses++;
+        }
+
+        echo "\n";
+    }
 }
 
-echo $resultText;
+echo 'Hits: ' . $hits . ' (rate: ' . ($hits/$totalAnomalies) . ")\n";
+echo 'Misses: ' . $misses . ' (rate: ' . ($misses/$totalAnomalies) . ")\n";
+echo 'False positive: ' . $falsesPositives . ' (rate: ' . ($falsesPositives/$totalBoth) . ")\n";
 
