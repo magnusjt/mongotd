693351a0bd91b4e43203de5b6213f7753cd693e6
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-04-17 15:33:37 +0200

    Connection can now be made with parameters or with an existing mongo client

--- src/Connection.php
+++ src/Connection.php
@@ -3,23 +3,52 @@
 use \MongoClient;
 
 class Connection{
-    private $client = null;
+    /** @var  MongoClient */
+    private $client;
 
-    private $db = null;
+    /** @var  \MongoDB */
+    private $db;
 
-    private $host = "localhost";
-
+    /** @var string  */
     private $dbName = "mongotd";
 
+    /** @var string  */
     private $collectionPrefix = "mongotd";
 
-    public function __construct($host = 'localhost', $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
-        $this->host              = $host;
-        $this->dbName           = $dbName;
+    private function __construct($client, $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
+        $this->client = $client;
+        $this->dbName = $dbName;
         $this->collectionPrefix = $collectionPrefix;
+        $this->db = $this->client->selectDB($dbName);
+    }
 
-        $this->client = new MongoClient("mongodb://{$this->host}");
-        $this->db = $this->client->selectDB($this->dbName);
+    /**
+     * @param string $host
+     * @param string $dbName
+     * @param string $collectionPrefix
+     *
+     * @return Connection
+     */
+    public static function fromParameters($host = 'localhost', $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
+        return new Connection(new MongoClient("mongodb://{$host}"), $dbName, $collectionPrefix);
+    }
+
+    /**
+     * @param MongoClient $mongoClient
+     * @param string      $dbName
+     * @param string      $collectionPrefix
+     *
+     * @return Connection
+     */
+    public static function fromClientConnection(MongoClient $mongoClient, $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
+        return new Connection($mongoClient, $dbName, $collectionPrefix);
+    }
+
+    /**
+     * @return MongoClient
+     */
+    public function getClient(){
+        return $this->client;
     }
 
     public function dropDb(){
