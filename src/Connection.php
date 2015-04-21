bf96f3b2cea06ffd1c64f972f272bb448c31b358
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-04-21 11:07:55 +0200

    Removed ability to supply mongoclient to connection, as it isnt safe. Also some formatting and stuff.

--- src/Connection.php
+++ src/Connection.php
@@ -1,6 +1,8 @@
 <?php namespace Mongotd;
 
 use \MongoClient;
+use \MongoDB;
+use \MongoCollection;
 
 class Connection{
     /** @var  MongoClient */
@@ -10,56 +12,42 @@ class Connection{
     private $db;
 
     /** @var string  */
-    private $dbName = "mongotd";
-
-    /** @var string  */
     private $collectionPrefix = "mongotd";
 
-    private function __construct($client, $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
-        $this->client = $client;
-        $this->dbName = $dbName;
+    public function __construct($host = 'localhost', $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
+        $this->client = new MongoClient("mongodb://{$host}");
         $this->collectionPrefix = $collectionPrefix;
         $this->db = $this->client->selectDB($dbName);
     }
 
     /**
-     * @param string $host
-     * @param string $dbName
-     * @param string $collectionPrefix
-     *
-     * @return Connection
-     */
-    public static function fromParameters($host = 'localhost', $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
-        return new Connection(new MongoClient("mongodb://{$host}"), $dbName, $collectionPrefix);
-    }
-
-    /**
-     * @param MongoClient $mongoClient
-     * @param string      $dbName
-     * @param string      $collectionPrefix
-     *
-     * @return Connection
-     */
-    public static function fromClientConnection(MongoClient $mongoClient, $dbName = 'mongotd', $collectionPrefix = 'mongotd'){
-        return new Connection($mongoClient, $dbName, $collectionPrefix);
-    }
-
-    /**
      * @return MongoClient
      */
-    public function getClient(){
+    public function client(){
         return $this->client;
     }
 
-    public function dropDb(){
-        $this->client->dropDB($this->dbName);
-    }
-
+    /**
+     * @return MongoDB
+     */
     public function db(){
         return $this->db;
     }
 
+    /**
+     * @param $collection string
+     *
+     * @return MongoCollection
+     */
     public function col($collection){
         return $this->db()->selectCollection($this->collectionPrefix . "_" . $collection);
+    }
+
+    /**
+     * @return string
+     */
+    public function lastError(){
+        $lastError = $this->db()->lastError();
+        return $lastError['err'];
     }
 }
